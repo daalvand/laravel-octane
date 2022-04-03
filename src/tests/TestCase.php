ba601fb50f8a751a25b08631284cebea6242f591
Mehdi Daalvand <mdaalvand@gmail.com>
2022-04-03 19:20:40 +0430

    add tests to project

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -2,9 +2,44 @@
 
 namespace Tests;
 
+use RuntimeException;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\File;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        if (!File::exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env.testing') ||
+            !app()->environment('testing')
+        ) {
+            throw new RuntimeException('file .env.testing not exists');
+        }
+
+        $this->setLaravelStart();
+        $this->createDatabaseFile();
+    }
+
+    /**
+     * @return void
+     */
+    protected function setLaravelStart(): void
+    {
+        if (!defined('LARAVEL_START')) {
+            define('LARAVEL_START', microtime(true));
+        }
+    }
+
+    /**
+     * @return void
+     */
+    protected function createDatabaseFile(): void
+    {
+        if (!File::exists(base_path('database/database.sqlite'))) {
+            File::put(base_path('database/database.sqlite'), '');
+        }
+    }
 }
